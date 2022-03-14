9f807ae20f9c47cb2e3e66dfda4aa13dbf729a22
Dakota Dunn <dakotadunn5@gmail.com>
2022-03-13 19:42:31 -0700

    able to add department

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const mysql = require("mysql2")
 const express = require("express")
 const console = require('console.table')
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
+const res = require('express/lib/response');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -16,3 +17,32 @@ const db = mysql.createConnection({
     password: process.env.MYSQL_PASSWORD,
     database: 'employees_db',
 });
+
+const whatWouldYouLikeToDo = () => {
+    inquirer.prompt([
+        {
+            name: 'whatWouldYouLikeToDo',
+            message: 'What would you like to do?',
+            type: "list",
+            choices: ["Add Department", "Add Role", 'Add Employee'];
+        }
+    ]).then((ans) => {
+        if(ans.whatWouldYouLikeToDo === "Add Department"){
+            inquirer.prompt([
+                {
+                    name: "nameOfDepartment",
+                    message: "What is the name of the department?",
+                    type: "input"
+                }.then((ans) => {
+                    const query = `INSERT INTO department(${ans});`;
+                    db.query(query, (err, result) => {
+                        if(err){
+                            return res.json(err)
+                        }
+                        res.json(result);
+                    })
+                })
+            ])
+        }
+    })
+}
