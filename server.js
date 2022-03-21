602ac6b7978ecb4955374127deac7a6054e2f25e
Dakota Dunn <dakotadunn5@gmail.com>
2022-03-20 21:32:24 -0700

    added the ability to add roles and departments as well as view everything, checked to make sure that all was functional so far

--- server.js
+++ server.js
@@ -39,24 +39,29 @@ function pickSomething() {
                 'Update employee role',
                 'Done!'
             ],
-            name: 'pick',
+            name: 'pickSomething',
             message: "Choose one of the following"
         }
     ]).then((ans) => {
         switch (ans.pickSomething){
             case 'View all Departments':
                 // initiate viewing departments
+                departments()
                 break;
             case 'View all roles':
                 // initiate viewing roles
+                roles()
                 break;
             case 'View all employees':
+                employees()
                 // initiate viewing employees
                 break;
             case 'Add a department':
+                addDepartment()
                 // initiate adding a department
                 break;
             case 'Add a role':
+                addRole();
                 // initiate adding a role
                 break;
             case 'Add employee':
@@ -72,8 +77,81 @@ function pickSomething() {
     });
 };
 
+const departments = () => {
+    let query = `SELECT * FROM department;`;
+    db.query(query, (err, res) => {
+        if(err) throw err;
+        console.table(res);
+        pickSomething()
+    })
+    
+}
 
+const roles = () => {
+    let query = `SELECT * FROM role;`;
+    db.query(query, (err,res) => {
+        if(err) throw err;
+        console.table(res);
+        pickSomething()
+    })
+    
+}
 
+const employees = () => {
+    let query  = `SELECT * FROM employee;`;
+    db.query(query, (err, res) => {
+        if(err) throw err;
+        console.table(res);
+        pickSomething()
+    })
+    
+}
+
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'department',
+            message: `Enter the name of the department you want to add`
+        }
+    ]).then((ans) => {
+        let query = `INSERT INTO department(name) VALUES (?);`;
+        db.query(query, [ans.department], (err, res) => {
+            if(err) throw err;
+            console.log(res);
+            pickSomething();
+        })
+    })
+}
+
+const addRole = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'role',
+            message: 'What is the name of the role?'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the salary of this role?'
+
+        },
+        {
+            type: 'number',
+            name: 'dept',
+            message: 'What department ID does this role belong to?'
+
+        }
+    ]).then((ans) => {
+        let query = `INSERT INTO role(title, salary, department_id)  VALUES (?, ?, ?);`;
+        db.query(query, [ans.role, ans.salary, ans.dept], (err,res) => {
+            if (err) throw err;
+            console.log(res);
+            pickSomething();
+        })
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`You are now listening on port ${PORT}`);
